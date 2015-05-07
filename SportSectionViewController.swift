9be3a70b5728645d15a80766e65647746027e081
AlucarDWeb <f3rdynand0>
2015-05-07 15:22:26 +0200

    Added UITabViewController

---
+++ SportSectionViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SportSectionViewController.swift
+//  Headlines
+//
+//  Created by Ferdinando Furci on 07/05/15.
+//  Copyright (c) 2015 The AppDate. All rights reserved.
+//
+
+import UIKit
+
+class SportSectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
