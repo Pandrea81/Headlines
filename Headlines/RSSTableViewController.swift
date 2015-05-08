1ac2e59f865b0f00d60a000aa9669c443909deda
AlucarDWeb <f3rdynand0>
2015-05-08 15:54:36 +0200

    Added more interaction and storyboard elements

--- Headlines/RSSTableViewController.swift
+++ Headlines/RSSTableViewController.swift
@@ -1,134 +1,103 @@
 //
-//  GenericListTableViewController.swift
+//  RSSTableViewController.swift
 //  Headlines
 //
-//  Created by Ferdinando Furci on 07/05/15.
+//  Created by Ferdinando Furci on 08/05/15.
 //  Copyright (c) 2015 The AppDate. All rights reserved.
 //
 
 import UIKit
 
+class RSSTableViewController: UITableViewController {
 
-class RSScell: UITableViewCell {
-    
-    
-    @IBOutlet weak var mImage: UIImageView!
-    @IBOutlet weak var mTitle: UILabel!
-    
-    
-    @IBOutlet weak var mDescription: UITextView!
-     override init(style: UITableViewCellStyle, reuseIdentifier: String!) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
-        
-    }
-
-    required init(coder aDecoder: NSCoder)
-    {
-        super.init(coder: aDecoder)
-    }
-    
-    
-}
-
-class GenericListTableViewController: UITableViewController {
-   
-   
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        
+     
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
         return 1
     }
-
+    
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
         return 1
     }
-
+    
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("myCell", forIndexPath: indexPath) as! RSScell
-
+        let cell = tableView.dequeueReusableCellWithIdentifier("RSScell", forIndexPath: indexPath) as! UITableViewCell
         
-        let image = UIImage(named: "repubblica")
+        cell.textLabel?.text = "YO"
         
-        cell.mTitle.text = "dadaddadadadadad"
-        cell.mDescription.text = "blablablablablablaahahahaagduaudgòJDajdaJFPajfòFJòlafjòLJAFòalj"
-        cell.mDescription.editable = false
-        cell.mDescription.userInteractionEnabled = true
-        cell.mImage.image = image
-
+        
         return cell
     }
-    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
-        self.performSegueWithIdentifier("mySegue", sender: self)
-    }
+   
     
-
+    
     /*
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the specified item to be editable.
-        return true
+    // Return NO if you do not want the specified item to be editable.
+    return true
     }
     */
-
+    
     /*
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
     }
     */
-
+    
     /*
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
+    
     }
     */
-
+    
     /*
     // Override to support conditional rearranging of the table view.
     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        // Return NO if you do not want the item to be re-orderable.
-        return true
+    // Return NO if you do not want the item to be re-orderable.
+    return true
     }
     */
-
+    
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         
         
     }
     
-
+    
     
 }
+
 
 
